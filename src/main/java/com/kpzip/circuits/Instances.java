7e6803e7d6de3be9f4252c414d4a68366288d6b9
kpzip <kpzipgames@gmail.com>
2021-12-25 20:01:19 -0800

    refactor and edit mod properties

---
+++ src/main/java/com/kpzip/circuits/Instances.java
@@ -0,0 +1,12 @@
+package com.kpzip.circuits;
+
+//Class for global variables
+public final class Instances {
+	
+	//Throw an Exception if for some reason someone tries to create an object of this class
+	public Instances() {
+		throw new IllegalStateException("Do not attempt to create an object of 'Instances.java'");
+	}
+	
+	public static final String MOD_ID = "circuits";
+}
