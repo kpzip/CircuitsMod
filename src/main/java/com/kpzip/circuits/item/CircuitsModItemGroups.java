cdc38cd2a70101e8ecc554e455f02a8e2e18ddd5
kpzip <kpzipgames@gmail.com>
2021-12-26 18:01:23 -0800

    updated to use NBT for resistance data

--- src/main/java/com/kpzip/circuits/item/CircuitsModItemGroups.java
+++ src/main/java/com/kpzip/circuits/item/CircuitsModItemGroups.java
@@ -6,7 +6,6 @@ import com.kpzip.circuits.util.Instances;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.Identifier;
 
 public final class CircuitsModItemGroups {
@@ -19,21 +18,12 @@ public final class CircuitsModItemGroups {
 			new Identifier(Instances.MOD_ID, "circuits_components")).icon(
 			() -> new ItemStack(CircuitsModItems.RESISTOR))
 			.appendItems(stacks -> {
+				//Add Resistors
+				ItemStack[] resistors = ItemResistor.getItemStacksForCreativeTab();
+				for (ItemStack e : resistors) {
+					stacks.add(e);
+				}
 				
-				ItemStack resistor = new ItemStack(CircuitsModItems.RESISTOR);
-				NbtCompound nbt = resistor.getNbt();
-				
-				NbtCompound nbt_resistor_unknown = nbt.copy();
-				nbt_resistor_unknown.putString(ItemResistor.RESISTANCE_NBT_KEY, "");
-				ItemStack resistor_unknown = resistor.copy();
-				resistor_unknown.setNbt(nbt_resistor_unknown);
-				stacks.add(resistor_unknown);
-				
-				NbtCompound nbt_resistor_1 = nbt.copy();
-				nbt_resistor_1.putString(ItemResistor.RESISTANCE_NBT_KEY, "1");
-				ItemStack resistor_1 = resistor.copy();
-				resistor_unknown.setNbt(nbt_resistor_1);
-				stacks.add(resistor_1);
 			})
 			.build();
 }
--- src/main/java/com/kpzip/circuits/item/customItems/ItemResistor.java
+++ src/main/java/com/kpzip/circuits/item/customItems/ItemResistor.java
@@ -2,10 +2,13 @@ package com.kpzip.circuits.item.customItems;
 
 import java.util.List;
 
+import com.kpzip.circuits.item.CircuitsModItems;
+
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Formatting;
@@ -15,19 +18,8 @@ public class ItemResistor extends Item {
 	
 	public static final String RESISTANCE_NBT_KEY = "resistance";
 	
-	/* Valid Resistance Values:
-	 * ""
-	 * "1"
-	 * "10"
-	 * "100"
-	 * "500"
-	 * "1k"
-	 * "5k"
-	 * "10k"
-	 * "100k"
-	 * "500k"
-	 * "1M"
-	 */
+	public static final int[] CREATIVE_TAB_RESISTANCES = new int[]{0, 1, 10, 100, 500, 1000};
+	
 
 	public ItemResistor(Settings settings) {
 		super(settings);
@@ -35,11 +27,76 @@ public class ItemResistor extends Item {
 	
 	@Override
 	public void appendTooltip(ItemStack stack, World world, List<Text> tooltip, TooltipContext context) {
-		NbtCompound nbt = stack.getNbt();
-		if (!nbt.contains(RESISTANCE_NBT_KEY)) {
-			nbt.putString(RESISTANCE_NBT_KEY, "");
+		
+		int resistance = getItemStackResistance(stack);
+		
+		if (resistance <= 0) {
+			tooltip.add(new TranslatableText("item.circuits.resistor.tooltip.0").formatted(Formatting.GRAY));
 		}
-		tooltip.add(new TranslatableText("item.circuits.resistor.tooltip.v" + nbt.getString(RESISTANCE_NBT_KEY)).formatted(Formatting.GRAY));
+		else {
+			String text = "";
+			if (resistance >= 1000000) {
+				text = String.valueOf(resistance/1000000.0f).substring(0, String.valueOf(resistance/1000000.0f).length() >= 4 ? 4 : String.valueOf(resistance/1000000.0f).length());
+				text += " MΩ";
+			}
+			else if (resistance >= 1000) {
+				text = String.valueOf(resistance/1000.0f).substring(0, String.valueOf(resistance/1000.0f).length() >= 3 ? 3 : String.valueOf(resistance/1000.0f).length());
+				text += " kΩ";
+			}
+			else {
+				text = String.valueOf(resistance);
+				text += " Ω";
+			}
+			tooltip.add(new LiteralText(text).formatted(Formatting.GRAY));
+		}
+		
+	}
+	
+	public static ItemStack setItemStackResistance(ItemStack stack, int resistance) {
+		if (stack.getItem() instanceof ItemResistor) {
+			NbtCompound nbt = stack.getNbt();
+			
+			//Make sure NBT is non-null to avoid null pointer exception
+			if (nbt == null) {
+				nbt = new NbtCompound();
+			}
+			nbt.putInt(RESISTANCE_NBT_KEY, resistance);
+			
+			return stack;
+		}
+		else {
+			throw new IllegalArgumentException("Cannot set the resistance of a non-resistor item.");
+		}
+	}
+	
+	public static int getItemStackResistance(ItemStack stack) {
+		if (stack.getItem() instanceof ItemResistor) {
+			NbtCompound nbt = stack.getNbt();
+			
+			//Make sure NBT is non-null to avoid null pointer exception
+			if (nbt == null) {
+				nbt = new NbtCompound();
+			}
+			if (!nbt.contains(RESISTANCE_NBT_KEY)) {
+				nbt.putInt(RESISTANCE_NBT_KEY, 0);
+			}
+			return nbt.getInt(RESISTANCE_NBT_KEY);
+		}
+		else {
+			throw new IllegalArgumentException("Cannot find the resistance of a non-resistor item.");
+		}
+	}
+	
+	public static ItemStack[] getItemStacksForCreativeTab() {
+		ItemStack[] items = new ItemStack[CREATIVE_TAB_RESISTANCES.length];
+		for (int i = 0; i > items.length; i++) {
+			NbtCompound nbt = new NbtCompound();
+			nbt.putString(ItemResistor.RESISTANCE_NBT_KEY, "");
+			ItemStack resistor = new ItemStack(CircuitsModItems.RESISTOR);
+			resistor.setNbt(nbt);
+			items[(items.length - 1) - i] = resistor;
+		}
+		return items;
 	}
 
 }
