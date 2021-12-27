406c475e09bcc57ebf492543ba0bd9eb3d3eb372
kpzip <kpzipgames@gmail.com>
2021-12-26 19:11:06 -0800

    fix up tooltips

--- src/main/java/com/kpzip/circuits/item/customItems/ItemResistor.java
+++ src/main/java/com/kpzip/circuits/item/customItems/ItemResistor.java
@@ -18,7 +18,7 @@ public class ItemResistor extends Item {
 	
 	public static final String RESISTANCE_NBT_KEY = "resistance";
 	
-	public static final int[] CREATIVE_TAB_RESISTANCES = new int[]{0, 1, 10, 100, 500, 1000};
+	public static final int[] CREATIVE_TAB_RESISTANCES = new int[]{0, 1, 10, 100, 500, 1000, 5000, 10000, 50000, 100000, 500000, 1000000, 5000000, 10000000};
 	
 
 	public ItemResistor(Settings settings) {
@@ -35,18 +35,37 @@ public class ItemResistor extends Item {
 		}
 		else {
 			String text = "";
+			
+			//Set metric prefixes
 			if (resistance >= 1000000) {
-				text = String.valueOf(resistance/1000000.0f).substring(0, String.valueOf(resistance/1000000.0f).length() >= 4 ? 4 : String.valueOf(resistance/1000000.0f).length());
+				text = String.valueOf(resistance/1000000.0f);
+				text = text.substring(0, text.length() >= 4 ? 4 : text.length());
 				text += " MΩ";
 			}
 			else if (resistance >= 1000) {
-				text = String.valueOf(resistance/1000.0f).substring(0, String.valueOf(resistance/1000.0f).length() >= 3 ? 3 : String.valueOf(resistance/1000.0f).length());
+				text = String.valueOf(resistance/1000.0f);
+				text = text.substring(0, text.length() >= 3 ? 3 : text.length());
 				text += " kΩ";
 			}
 			else {
 				text = String.valueOf(resistance);
 				text += " Ω";
 			}
+			
+			//remove the decimal point and anything after that to remove things like 10. and 10.0
+			String[] split = text.split("\\.");
+			if (split.length > 1) {
+				boolean doesNotNeedDecimal = true;
+				for (int i = 0; i < split[1].split(" ")[0].length(); i++) {
+					if (split[1].charAt(i) != '0') {
+						doesNotNeedDecimal = false;
+					}
+				}
+				if (doesNotNeedDecimal) {
+					text = split[0] + " " + split[1].split(" ")[1];
+				}
+			}
+			
 			tooltip.add(new LiteralText(text).formatted(Formatting.GRAY));
 		}
 		
@@ -59,6 +78,7 @@ public class ItemResistor extends Item {
 			//Make sure NBT is non-null to avoid null pointer exception
 			if (nbt == null) {
 				nbt = new NbtCompound();
+				stack.setNbt(nbt);
 			}
 			nbt.putInt(RESISTANCE_NBT_KEY, resistance);
 			
@@ -76,6 +96,7 @@ public class ItemResistor extends Item {
 			//Make sure NBT is non-null to avoid null pointer exception
 			if (nbt == null) {
 				nbt = new NbtCompound();
+				stack.setNbt(nbt);
 			}
 			if (!nbt.contains(RESISTANCE_NBT_KEY)) {
 				nbt.putInt(RESISTANCE_NBT_KEY, 0);
@@ -89,10 +110,12 @@ public class ItemResistor extends Item {
 	
 	public static ItemStack[] getItemStacksForCreativeTab() {
 		ItemStack[] items = new ItemStack[CREATIVE_TAB_RESISTANCES.length];
-		for (int i = 0; i > items.length; i++) {
-			NbtCompound nbt = new NbtCompound();
-			nbt.putString(ItemResistor.RESISTANCE_NBT_KEY, "");
-			ItemStack resistor = new ItemStack(CircuitsModItems.RESISTOR);
+		NbtCompound nbt;
+		ItemStack resistor;
+		for (int i = 0; i < items.length; i++) {
+			nbt = new NbtCompound();
+			nbt.putInt(ItemResistor.RESISTANCE_NBT_KEY, CREATIVE_TAB_RESISTANCES[i]);
+			resistor = new ItemStack(CircuitsModItems.RESISTOR, 1);
 			resistor.setNbt(nbt);
 			items[(items.length - 1) - i] = resistor;
 		}
